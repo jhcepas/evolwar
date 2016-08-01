5bdd7534adb48279c087b3fb52dfd1d41f300aeb
Jaime Huerta-Cepas <jhcepas@gmail.com>
2016-07-31 18:23:10 -0700

    basic structure created

---
+++ cwevolver/mutate.py
@@ -0,0 +1,8 @@
+# functions to evolve the code of warriors
+
+def mut1(w):
+    pass
+
+
+    
+
---
+++ evolve.py
@@ -0,0 +1,14 @@
+from argparse import ArgumentParser
+
+from cwevolver import mutate as mut
+
+# command line tool to evolve warriors...
+def main(args):
+    pass
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser('Evolve corewar warrriors')
+    args = parser.parse_args()
+    
+    main(args)
