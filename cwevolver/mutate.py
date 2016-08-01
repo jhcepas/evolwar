01399b15cc9f796143005d1d00272a9e8e22c384
Jaime Huerta-Cepas <jhcepas@gmail.com>
2016-07-31 22:04:15 -0700

    fix mutator

--- cwevolver/mutate.py
+++ cwevolver/mutate.py
@@ -1,38 +1,40 @@
+import numpy as np
 from collections import namedtuple
 from random import randint, random, choice
 from redcode import parse, OPCODES, MODIFIERS, MODES, Instruction
 
-def duplicate(warrior, dup_size, dup_dist):
-    start = randint(0, len(warrior))
+def duplicate(instructions, dup_size, dup_dist):
+    start = randint(0, len(instructions))
     end = randint(start+1, start+dup_size+1)
     insert_point = randint(start+1, start+dup_dist)
-    warrior[insert_point:insert_point] = warrior[start:end]
+    instructions[insert_point:insert_point] = instructions[start:end]
 
-def delete(warrior, loss_size):
-    start = randint(0, len(warrior))
+def delete(instructions, loss_size):
+    start = randint(0, len(instructions))
     end = randint(start+1, start+loss_size+1)
-    warrior[start:end] = []
+    instructions[start:end] = []
     
-def evolver_1(w):
-    warrior = parse(w)
+def evolver_1(warrior):
+    #mutator = np.array([0.01]*8)
+    mutator = [0.005, 0.001, 0.001, 0.01, 0.01, 0.001, 0.001, 0.001]
     instructions = warrior.instructions
     
     # Duplications
-    prob_duplication = 0.01
+    prob_duplication = mutator[0]
     dup_size_values = range(1, 10)
     dup_distance_values = range(1, 10)
     if random() < prob_duplication:
         dup_size = choice(dup_size_values)
-        dup_distance = choice(dup_distance_values)        
+        dup_dist = choice(dup_distance_values)        
         duplicate(instructions, dup_size, dup_dist)
 
     # Deletions
-    prob_deletion = 0.01
+    prob_deletion = mutator[1]
     del_size = range(1, 10)    
     if random() < prob_deletion:
-        dup_size = choice(dup_size_values)
-        dup_distance = choice(dup_distance_values)        
-        delete(instructions, dup_size, dup_dist)
+        loss_size = choice(dup_size_values)
+        loss_dist = choice(dup_distance_values)        
+        delete(instructions, loss_size)
 
     # Point mutations
     position_values = {
@@ -43,19 +45,15 @@ def evolver_1(w):
         4: MODES,
         5: range(-10, 10),
     }
-    inst_index = randint(0, len(instructions)-1)
-    prob_mutation = [0.9, 0.01, 0.01, 0.01, 0.01, 0.01]
-    inst = list(instructions[inst_index])
-    for i, mut_rate in enumerate(prob_mutation):
-        if random() < mut_rate:
-            values = position_values[i]
-            inst[i] = choice(values)
-    instructions[inst_index] = Instruction(*inst)    
-    print warrior.instructions
+
+    if len(instructions):
+        inst_index = randint(0, len(instructions)-1)
+        prob_mutation = mutator[2:8]
+        inst = list(instructions[inst_index])
+        for i, mut_rate in enumerate(prob_mutation):
+            if random() < mut_rate:
+                values = position_values[i]
+                inst[i] = choice(values)
+        instructions[inst_index] = Instruction(*inst)
+
     return warrior
-
-
-
-        
-
-
--- evolve.py
+++ evolve.py
@@ -1,12 +1,21 @@
 from argparse import ArgumentParser
 
 from cwevolver import mutate as mut
+from cwevolver import redcode
 
 # command line tool to evolve warriors...
 def main(args):
-    for warrior_file in args.warriors:
-        mut.evolver_1(warrior_file)
 
+    generations = 1000
+    for warrior_file in args.warriors:
+        warrior = redcode.parse(warrior_file)
+        for i in xrange(generations):
+            mut.evolver_1(warrior)
+            
+        for inst in warrior.instructions:
+            print "%s.%s %s%s %s%s" %inst
+
+            
 if __name__ == '__main__':
     parser = ArgumentParser('Evolve corewar warrriors')
     parser.add_argument('warriors', metavar='warriors', type=str, nargs='+')                         
